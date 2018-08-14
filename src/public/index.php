901b908d4e602c8c9a56d9e898860da1e9f70cff
Shadman Jamil <shadman.jamil@tenpearls.com>
2018-08-14 15:26:23 +0500

    added html page to render a board

---
+++ src/public/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- script src="https://code.jquery.com/jquery-3.3.1.min.js?v=1.0"></script -->
+	<script src="assets/jquery-3.3.1.min.js?v=1.0"></script>
+	<script src="assets/boardgame.js?v=1.0"></script>
+
+	<link rel="stylesheet" href="assets/boardgame.css?v=1.0">
+</head>
+<body>
+	<h1>Tic Tac Toe with Bot</h1>
+	<form id="boardgame">
+		<div id="board"></div>
+	</form>
+</body>
+</html>
